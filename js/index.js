64d2ebe94bb2b5098ea2e1be999b91dd142dd143
Krystyna Shunkina <christina.shunkina@gmail.com>
2024-01-11 17:04:24 +0200

    hw16 1st try

---
+++ js/index.js
@@ -0,0 +1,40 @@
+const titleInput = document.getElementById('title');
+const textInput = document.getElementById('text');
+const submitBtn = document.getElementById('submitBtn');
+const customAlert = document.getElementById('customAlert');
+const trueAlert = document.getElementById('alert');
+const alerTitle = document.getElementById('alerTitle');
+const alerText = document.getElementById('alerText');
+const alertBtn = document.getElementById('alertBtn');
+submitBtn.addEventListener('click', () => {
+    const title = titleInput.value.trim();
+    const text = textInput.value.trim();
+    if (text !== "" && title !== "") {
+        addText(title, text);
+        showAlert(customAlert, trueAlert);
+        titleInput.value = "";
+        textInput.value = "";
+    }
+    else {
+        alert('Nothig is typed.')
+    }
+}
+
+)
+function showAlert(wrapper, al) {
+
+    wrapper.classList.add('active');
+    al.classList.add('active');
+
+}
+function addText(title, text) {
+    alerTitle.textContent = title;
+    alerText.textContent = text;
+}
+
+alertBtn.addEventListener('click', () => {
+    customAlert.classList.remove('active');
+    trueAlert.classList.remove('active');
+}
+
+)
